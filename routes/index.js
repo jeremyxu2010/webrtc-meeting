893dd6f9e51b9bb96d32b7725a7b65d7ef1ca57d
Jeremy Xu <jeremyxu2010@gmail.com>
2013-10-13 19:23:29 +0800

    自动生成currentUser

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,17 @@
+var uuid = require('uuid');
+
 module.exports = function (app) {
   /*
   * GET home page.
   */
   app.get("/", function (req, res) {
+    var currentUser = req.query.user;
+    if (!currentUser) {
+      currentUser = uuid.v4();
+    };
     res.render('index', {
-      title: 'Express',
-      user: req.query.user
+      title: 'webrtc-meeting',
+      user: currentUser
     });
   });
 };
