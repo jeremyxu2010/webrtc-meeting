c25466bf9a937b60a0f55268dcca39ce96df8caf
Jeremy Xu <jeremyxu2010@gmail.com>
2013-10-15 19:28:27 +0800

    use my own turnserver

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -44,10 +44,15 @@ require.config({
 });
 
 function start($, io, HashTable) {
-  var iceServers = [{
-      "url": "stun:stun.ekiga.net:3478?transport=tcp"
-    }, {
-      "url": "turn:test@74.117.58.198:80?transport=tcp",
+  var iceServers = [/*{
+      "url": "stun:stun.l.google.com:19302?transport=tcp"
+    },{
+      "url": "stun:119.145.0.153:80?transport=tcp",
+      "username": "test",
+      "credential": "123456"
+    },*/{
+      "url": "turn:119.145.0.153:80?transport=tcp",
+      "username": "test",
       "credential": "123456"
     }];
 
@@ -81,8 +86,89 @@ function start($, io, HashTable) {
 
     var pcs = new HashTable();
 
+    var bandwidth = {
+      "audio" : 0,
+      "video": 56,
+      "data": 0
+    };
+
+    function setBandwidth(sdp) {
+      if (!bandwidth) {
+        return;
+      }
+
+      // remove existing bandwidth lines
+      sdp = sdp.replace(/b=AS([^\r\n]+\r\n)/g, '');
+
+      if (bandwidth.audio) {
+        sdp = sdp.replace(/a=mid:audio\r\n/g, 'a=mid:audio\r\nb=AS:' + bandwidth.audio + '\r\n');
+      }
+
+      if (bandwidth.video) {
+        sdp = sdp.replace(/a=mid:video\r\n/g, 'a=mid:video\r\nb=AS:' + bandwidth.video + '\r\n');
+      }
+
+      if (bandwidth.data) {
+        sdp = sdp.replace(/a=mid:data\r\n/g, 'a=mid:data\r\nb=AS:' + bandwidth.data + '\r\n');
+      }
+
+      return sdp;
+    }
+
+    var framerate = {
+      "minptime" : 5,
+      "maxptime" : 255
+    };
+
+    function setFramerate(sdp) {
+      sdp = sdp.replace('a=fmtp:111 minptime=10', 'a=fmtp:111 minptime=' + (framerate.minptime || 10));
+      sdp = sdp.replace('a=maxptime:60', 'a=maxptime:' + (framerate.maxptime || 60));
+      return sdp;
+    }
+
+    function setBitrate(sdp) {
+        //sdp = sdp.replace( /a=mid:video\r\n/g , 'a=mid:video\r\na=rtpmap:120 VP8/90000\r\na=fmtp:120 x-google-min-bitrate=' + (bitrate || 10) + '\r\n');
+        return sdp;
+    }
+
+    function getInteropSDP(sdp) {
+      var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''),
+        extractedChars = '';
+
+      function getChars() {
+        extractedChars += chars[parseInt(Math.random() * 40)] || '';
+        if (extractedChars.length < 40)
+          getChars();
+
+        return extractedChars;
+      }
+
+      // for audio-only streaming: multiple-crypto lines are not allowed
+      /*
+      if (options.onAnswerSDP)
+        sdp = sdp.replace( /(a=crypto:0 AES_CM_128_HMAC_SHA1_32)(.*?)(\r\n)/g , '');
+      */
+
+      var inline = getChars() + '\r\n' + (extractedChars = '');
+      sdp = sdp.indexOf('a=crypto') == -1 ? sdp.replace( /c=IN/g ,
+        'a=crypto:1 AES_CM_128_HMAC_SHA1_80 inline:' + inline +
+            'c=IN') : sdp;
+
+      return sdp;
+    }
+
+    function serializeSdp(sdp) {
+      //if (moz) return sdp;
+      sdp = setBandwidth(sdp);
+      sdp = setFramerate(sdp);
+      sdp = setBitrate(sdp);
+      sdp = getInteropSDP(sdp);
+      return sdp;
+    }
+
     function offer(pc, user) {
       pc.createOffer(function (description) {
+        description.sdp = serializeSdp(description.sdp);
         pc.setLocalDescription(description);
         socket.emit("message", {
           "fromuser": currentUser,
@@ -174,6 +260,7 @@ function start($, io, HashTable) {
           if (pc) {
             pc.setRemoteDescription(new RTCSessionDescription(data.description));
             pc.createAnswer(function (description) {
+              description.sdp = serializeSdp(description.sdp);
               pc.setLocalDescription(description);
               socket.emit("message", {"fromuser": currentUser, "touser": data.fromuser, type: "answer", description: description});
             });
@@ -198,6 +285,6 @@ function start($, io, HashTable) {
 
 //requiring the scripts in the first argument and then passing the library namespaces into a callback
 //you should be able to console log all of the callback arguments
-require(['jquery', 'socketio', 'hashtable', 'jquery.bootstrap', 'css!libs/bootstrap/dist/css/bootstrap', 'less!../stylesheets/index'], function ($, io, HashTable) {
+require(['jquery', 'socketio', 'hashtable', /*'jquery.bootstrap', 'css!libs/bootstrap/dist/css/bootstrap', */'less!../stylesheets/index'], function ($, io, HashTable) {
   start($, io, HashTable);
 });
